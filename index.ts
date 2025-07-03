92fb9e017295e121445ec1682b2f39d277f29eca
LucasLaguilio <laguiliolucas@gmail.com>
2025-07-03 17:36:56 -0400

    Alterações inicias no BackEnd, adicionando queries de cadastro e listagem.

---
+++ index.ts
@@ -0,0 +1,87 @@
+import mysql, { Connection, ConnectionOptions , QueryError } from 'mysql2/promise';
+import fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import cors from '@fastify/cors'
+import { console } from 'inspector';
+
+const app = fastify()
+app.register(cors)
+
+app.get("/", (request: FastifyRequest, reply: FastifyReply) => {
+    reply.send("Fastify Funcionando!")
+})
+app.get("/Carros", async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+        const conn = await mysql.createConnection({
+            host: "localhost",
+            user: 'root',
+            password: "root",
+            database: 'AutoStar',
+            port: 3306
+        });
+        const resultado = await conn.query("SELECT * FROM Carros")
+        const [dados,estruturaTabela] = resultado
+        reply.status(200).send(dados)
+        
+    } catch (erro:any) {
+        if (erro.code === "ECONNREFUSED") {
+            console.log("ERRO: LIGUE O LARAGÃO!!! CABEÇA!")
+            reply.status(400).send({mensagem:"ERRO: LIGUE O LARAGÃO!!! CABEÇA!"})
+        } else if (erro.code === "ER_BAD_DB_ERROR") {
+            console.log("ERRO: CONFIRA O NOME DO BANCO DE DADOS OU CRIE UM NOVO BANCO COM O NOME QUE VOCÊ COLOCOU LÁ NA CONEXÃO")
+            reply.status(400).send({mensagem:"ERRO: CONFIRA O NOME DO BANCO DE DADOS OU CRIE UM NOVO BANCO COM O NOME QUE VOCÊ COLOCOU LÁ NA CONEXÃO"})
+        } else if (erro.code === "ER_ACCESS_DENIED_ERROR") {
+            console.log("ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO")
+            reply.status(400).send({mensagem:"ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO"})
+        } else {
+            console.log(erro)
+            reply.status(400).send({mensagem:"ERRO DESCONHECIDO OLHE O TERMINAL"})
+        }
+    }
+})
+app.post("/Carros", async (request: FastifyRequest, reply: FastifyReply) => {
+    const {id,nome,marca,preco,cor,categoria,ano,descricao,linkimg }  = request.body as any
+    try {
+        const conn = await mysql.createConnection({
+            host: "localhost",
+            user: 'root',
+            password: "root",
+            database: 'AutoStar',
+            port: 3306
+        });
+        const resultado = 
+        await conn.query("INSERT INTO Carros (id,nome,marca,preco,cor,categoria,ano,descricao,linkimg ) VALUES (?,?,?,?,?,?,?,?,? )",
+                                                                    [id,nome,marca,preco,cor,categoria,ano,descricao,linkimg])
+        const [dados,estruturaTabela] = resultado
+        reply.status(200).send(resultado)
+        
+    } catch (erro:any) {
+        switch (erro.code) {
+            case "ECONNREFUSED":
+                console.log("ERRO: LIGUE O LARAGÃO!!! CABEÇA!");
+                reply.status(400).send({ mensagem: "ERRO: LIGUE O LARAGÃO!!! CABEÇA!" });
+                break;
+            case "ER_BAD_DB_ERROR":
+                console.log("ERRO: CONFIRA O NOME DO BANCO DE DADOS OU CRIE UM NOVO BANCO COM O NOME QUE VOCÊ COLOCOU LÁ NA CONEXÃO");
+                reply.status(400).send({ mensagem: "ERRO: CONFIRA O NOME DO BANCO DE DADOS OU CRIE UM NOVO BANCO COM O NOME QUE VOCÊ COLOCOU LÁ NA CONEXÃO" });
+                break;
+            case "ER_ACCESS_DENIED_ERROR":
+                console.log("ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO");
+                reply.status(400).send({ mensagem: "ERRO: CONFIRA O USUÁRIO E SENHA NA CONEXÃO" });
+                break;
+            case "ER_DUP_ENTRY":
+                console.log("ERRO: VOCÊ DUPLICOU A CHAVE PRIMÁRIA");
+                reply.status(400).send({ mensagem: "ERRO: VOCÊ DUPLICOU A CHAVE PRIMÁRIA" });
+                break;
+            default:
+                console.log(erro);
+                reply.status(400).send({ mensagem: "ERRO DESCONHECIDO OLHE O TERMINAL DO BACKEND" });
+                break;
+        }
+    }
+})
+app.listen({ port: 8000 }, (erro, endereco) => {
+    if (erro) {
+        console.log("ERRO: Fastify não iniciou")
+    }
+    console.log(`Fastify iniciado na porta: ${endereco}`)
+})
