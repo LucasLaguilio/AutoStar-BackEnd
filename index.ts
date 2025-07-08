b6e05eab12f644c6e7dd79e9368c2bd0f2d63ac7
Lucas Lio <laguiliolucas@gmail.com>
2025-07-08 08:21:05 -0400

    Alteração Porta para 8001

--- index.ts
+++ index.ts
@@ -79,7 +79,7 @@ app.post("/Carros", async (request: FastifyRequest, reply: FastifyReply) => {
         }
     }
 })
-app.listen({ port: 8000 }, (erro, endereco) => {
+app.listen({ port: 8001 }, (erro, endereco) => {
     if (erro) {
         console.log("ERRO: Fastify não iniciou")
     }
